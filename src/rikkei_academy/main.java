d492438b494fd44972ca959857a249cc401653bb
hongtdit <trandinhhong1997@gmail.com>
2023-03-05 19:32:20 +0700

    m

---
+++ src/rikkei_academy/main.java
@@ -0,0 +1,19 @@
+package rikkei_academy;
+
+public class main {
+    public static void main(String[] args) {
+        // write your code here
+        Student.change(); //calling change method
+
+        //creating objects
+        Student s1 = new Student(111, "Chinh");
+        Student s2 = new Student(222, "Toan");
+        Student s3 = new Student(333, "Dung");
+
+        //calling display method
+        s1.display();
+        s2.display();
+        s3.display();
+
+    }
+}
